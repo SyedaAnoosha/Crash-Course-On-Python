96185e05f9006b3f35e452171c385b7b888a38bd
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-16 20:43:47 +0500

    commit

---
+++ String printout.py
@@ -0,0 +1,12 @@
+# The following 3 lines assign strings to a list of variables.
+
+first_name = "Syeda"
+middle_name = "Anoosha"
+last_name = "Iqtidar"
+ 
+print(first_name + " " + middle_name + " " + last_name)
+ 
+# Alternatively, you could use commas in place of the + connector:
+print(first_name, middle_name, last_name)
+
+# However, you will find that this produces a space before a comma within a string.
