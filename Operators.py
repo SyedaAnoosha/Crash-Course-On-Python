2e5e17b4f1780e1a6d95e1ac5b71afb05e165250
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-17 01:32:32 +0500

    commit

--- Operators.py
+++ Operators.py
@@ -32,8 +32,20 @@ print("sunbathe" > "suntan") #False because 'b' does not have a higher Unicode v
 # If two identical strings are compared using the less than < comparison operator, this will produce a False result because they are equal.
 print("Lima" < "Lima") #False beacuse both the strings are same
 
-print("Five" < 6) #TypeError: '<' not supported between instances of 'str' and 'int'
+#print("Five" < 6) #TypeError: '<' not supported between instances of 'str' and 'int'
 
 
 # Logical Operators
-print((6*3 >= 18) and (9+9 <= 36/2))
+print((6*3 >= 18) and (9+9 <= 36/2)) # True
+print("Nairobi" < "Milan" and "Nairobi" > "Hanoi") #False
+
+
+x = 2*3 > 6
+print("The value of x is:")
+print(x)
+
+print("The inverse value of x is:")
+print(not x)
+
+print("A">"B" or "B"<"F") #True
+print(16 <= 4**2 or 9**(0.5) != 3) #True
