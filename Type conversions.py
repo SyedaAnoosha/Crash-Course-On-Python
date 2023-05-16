b4fa20b68fab04247463f9187b069559bbf7e1c8
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-16 20:25:12 +0500

    changes in docx and type conversion file

---
+++ Type conversions.py
@@ -0,0 +1,8 @@
+# manual coversion : explicit conversion
+roll_no = 4
+batch = "21"
+string_value = str(roll_no)
+int_valur = int(batch)
+
+# Python does itself for int nad float data types: implicit conversion
+print(7 + 8.5)
