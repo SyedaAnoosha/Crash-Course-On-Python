a846a6b4a21bef340e72b84121cf7119ab61a792
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-17 01:00:49 +0500

    Commit

---
+++ Conditionals.py
@@ -0,0 +1,18 @@
+# ==    (equality)  
+# !=     (not equal to)  
+# >       (greater than)
+# <      (less than)
+# >=    (greater than or equal to)
+# <=    (less than or equal to)
+
+# Comparison Operators with Numbers
+print(32 == 30+2) # True
+print(9/3 != 3*1) # False
+
+# Comparison Operators with Strings
+print("a string" == "a string") #True
+
+print("4 + 5" == 4 + 5) #False
+
+event_city = "Hyd"
+print(event_city != "Hyd") #False
