da75d50d52e81429d6d3465dbd00e6916cd66104
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-18 16:31:17 +0500

    commit

---
+++ OOP.py
@@ -0,0 +1,20 @@
+class Fruit:
+    flavor = ""
+    color = ""
+
+    # def __init__(self):
+    #     pass
+
+    def __init__ (self, color, flavor): # constructor
+        self.flavor = flavor
+        self.color = color
+
+    def __str__(self): #conversion to string.
+        return("This fruit is {} and its flavor is {}".format(self.color,self.flavor))
+        
+
+mango = Fruit("Yellow","Sweet")
+
+print(mango.color)
+print(mango.flavor)
+print(mango)
