53aab8e7c72a3af08f19d28975551b7829c8acb7
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-19 00:45:15 +0500

    commit

--- Type conversions.py
+++ Type conversions.py
@@ -2,7 +2,7 @@
 roll_no = 4
 batch = "21"
 string_value = str(roll_no)
-int_valur = int(batch)
+int_value = int(batch)
 
 # Python does itself for int nad float data types: implicit conversion
 print(7 + 8.5)
