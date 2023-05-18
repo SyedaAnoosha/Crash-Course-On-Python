fd5b4fd40b185f05ba944b36001bf0c9f8ce6271
SyedaAnoosha <anushazaidi06@gmail.com>
2023-05-18 16:50:33 +0500

    commit

--- Hello world.py
+++ Hello world.py
@@ -11,3 +11,11 @@ print(type(height))
 # <class 'str'>  
 # <class 'int'>  
 # <class 'float'>
+
+x = 12
+# x is a real number, so its imaginary part is 0.
+print(x.imag)
+# Here's how to make a complex number, in case you've ever been curious:
+c = 12 + 3j
+print(c.imag)
+print(c.real)
